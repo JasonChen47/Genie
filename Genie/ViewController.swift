28acf311443a001a26ce29478dfdaaf70c2f0788
JasonChen47 <42682065+JasonChen47@users.noreply.github.com>
2021-12-22 16:21:13 -0500

    Initial Commit

---
+++ Genie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Genie
+//
+//  Created by John Smith on 12/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
