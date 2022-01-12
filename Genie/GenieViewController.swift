b9a651f3a12567fe9a4249c65c9d47ca2a445c54
JasonChen47 <42682065+JasonChen47@users.noreply.github.com>
2022-01-12 14:50:52 -0500

    figured out unwind segue

---
+++ Genie/GenieViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GenieViewController.swift
+//  Genie
+//
+//  Created by John Smith on 1/12/22.
+//
+
+import UIKit
+
+class GenieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func myUnwindActionGenie(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Genie/ViewController.swift
+++ Genie/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {
+    @IBAction func myUnwindActionSignIn(unwindSegue: UIStoryboardSegue) {
         
     }
 
